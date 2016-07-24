720d935f6b22d4c7a7c16fae26e942d1cc0ddf70
bruno <brunomendez88@hotmail.com>
2016-07-24 11:28:13 -0300

    borre servicio que estaban de mas, modigicaciones en el web.xml pero todavia no se levatanta el home con la configuracion actual.

--- src/main/webapp/assets/js/utils.js
+++ src/main/webapp/assets/js/utils.js
@@ -2,7 +2,3 @@ var authTemplates = [
     { name: 'login', url: 'views/templates/login.html'},
     { name: 'loggedInUser', url: 'views/templates/loggedInUser.html'}
 ];
-
-var splashes = [
-    { name: 'splashFacebook', url: 'views/templates/splashNotLogged.html'}
-];
