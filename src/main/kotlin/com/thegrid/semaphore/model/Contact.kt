1c1d5bff89c17e317f9c0aef06e5d2a540d46bb7
surakituaka <meli_lucas_1104@hotmail.com>
2016-07-26 13:25:17 -0300

    el commit contiene: la biblioteca de spek como dependencia local en grandle

--- src/main/kotlin/com/thegrid/semaphore/model/Contact.kt
+++ src/main/kotlin/com/thegrid/semaphore/model/Contact.kt
@@ -1,6 +1,11 @@
 package com.thegrid.semaphore.model
 
+import org.jetbrains.spek.api.Spek
 import kotlin.*
+//import com.winterbe.expekt.expect
+//import com.winterbe.expekt.should
+//import org.jetbrains.spek.api.Spek
+
 /**
  * Created by crist on 6/7/2016.
  */
@@ -11,6 +16,24 @@ data class Contact(val name: String, val age: Int, var telephones: MutableList<T
     fun removeTelephoneByNumber(number: String) = telephones.remove(telephones.find { it.number ==  number})
 
     fun removeTelephoneByName(name: String) = telephones.remove(telephones.find { it.name ==  name})
+
 }
 
 data class Telephone(val number: String, val name: String)
+
+
+//ejemplo
+class SimpleTest : Spek({
+    describe("a contact") {
+        var tels: MutableList<Telephone> = mutableListOf()
+        val tel = Telephone("12345","tel de juan")
+        val contact = Contact("juan",23,tels)
+
+        it("should return the tel number") {
+            contact.addTelephone(tel)
+            println(contact.telephones.first().number)
+            assert(contact.telephones.first().number == "12345")
+        }
+
+    }
+})
