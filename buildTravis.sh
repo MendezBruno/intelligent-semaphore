7e2e4a9dea77b06ae887617b02c34e0b33dadb47
Cristian Erik Ames Masek <cristianames.92@gmail.com>
2016-07-29 02:18:36 -0300

    Adding the stack trace to travis tests

--- buildTravis.sh
+++ buildTravis.sh
@@ -3,7 +3,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew test
+  ./gradlew test --info --stacktrace
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
 fi
