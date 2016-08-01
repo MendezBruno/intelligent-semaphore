41472378cfbfcac7b2c5e49f4ad645feb0ea3426
mariano <marianoguido@yahoo.com.ar>
2016-08-01 01:13:09 -0300

    Arreglado error de calles horizontales y verticales infinitas

--- src/main/webapp/assets/js/controllers/editorController.js
+++ src/main/webapp/assets/js/controllers/editorController.js
@@ -9,11 +9,15 @@ app.controller('editorController', function($scope,logica) {
     stage = new createjs.Stage("mapa");
     logica.crearGrilla(4,4,30,stage);
     stage.update();
+    logica.modificarGrilla(5,5,30,stage);
+    stage.update();
+    logica.modificarGrilla(3,3,30,stage);
+    stage.update();
 //    createjs.Ticker.on("tick", stage);
 
-    $scope.ver = function($event) {
+    $scope.ver = function() {
         console.log($scope.callesV);
-        logica.modificarGrilla($scope.callesV,$scope.callesH,30,stage);
+        logica.modificarGrilla(parseInt($scope.callesV),parseInt($scope.callesH),30,stage);
 
         stage.update();
 
