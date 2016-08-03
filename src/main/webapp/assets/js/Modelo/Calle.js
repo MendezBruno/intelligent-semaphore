af65be518e2fde69251691e5a590fb5a54c9be8b
Ezequiel Ayzenberg <Ezequiel Ayzenberg>
2016-08-03 13:54:20 -0300

    Herencia de calles (modelo)

--- src/main/webapp/assets/js/Modelo/Calle.js
+++ src/main/webapp/assets/js/Modelo/Calle.js
@@ -9,11 +9,19 @@ var Sentido = {
     ESTE_OESTE: "Este-Oeste"
 }
 
-function Calle() {
+function Calle() { //ABSTRACTA
     this.cantCarriles = 2;
     this.fdpEntradaCantMax=1;
     this.fdpEntradaIntervalo=1;
     this.fdpSalidaCantMax=1;
     this.fdpSalidaIntervalo=1;
     this.sentido=undefined;
+}
+
+function CalleVertical() {
+    this.sentidosPosibles = [Sentido.NORTE_SUR, Sentido.SUR_NORTE];
+}
+
+function CalleHorizontal() {
+    this.sentidosPosibles = [Sentido.ESTE_OESTE, Sentido.OESTE_ESTE];
 }
