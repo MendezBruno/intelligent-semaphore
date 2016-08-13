fe328423250ff1cbe120e438bd86f446ca90238e
Ezequiel Ayzenberg <Ezequiel Ayzenberg>
2016-08-13 09:35:44 -0300

    Limpio y corrijo codigo

--- src/main/webapp/assets/js/canvas/logicaReproductor.js
+++ src/main/webapp/assets/js/canvas/logicaReproductor.js
@@ -4,7 +4,7 @@
 function ReproductorController(modelo, stage, $scope){
 
     this.modelo = modelo;
-    this.largo = 50;        //*TODO* sacar el largo de la calle como dato general
+    this.largo = 300;        //*TODO* sacar el largo de la calle como dato general
     this.ancho = 20         //*TODO* ver tambien la posibilidad que sea un dato general
     this.stage = stage;
 
@@ -14,8 +14,8 @@ ReproductorController.prototype.dibujar = function (){
 
     var self = this;
     var stage = self.stage;
-    var posInicialX = 20;   //en un futuro se podria parametrizar
-    var posInicialY = 20;   //en un futuro se podria parametrizar
+    var posInicialX = 0;   //en un futuro se podria parametrizar
+    var posInicialY = 0;   //en un futuro se podria parametrizar
     var posx = posInicialX;
     var posy = this.largo+posInicialY;
     var separador = 20;
@@ -37,62 +37,58 @@ ReproductorController.prototype.dibujar = function (){
     var horizontales = this.modelo.callesHorizontales;
     var verticales = this.modelo.callesVerticales;
     for (var i=0; i<horizontales.length; i++) {
+        var calle = horizontales[i];
+        var cantCarriles = calle.cantCarriles;
+        var cuadras = calle.cuadras;
         moverPosxAlOrigen();
-        for (var  j = 0; j < verticales.length; j++) {
-            //generarCuadra(HORIZONTAL,horizontales[i].cantCarriles);
-            this.stage.addChild(new CnvCuadraReproductor(posx,posy,largo,horizontales[i].cantCarriles,"#b3b3b3",HORIZONTAL));
-            actualizarPosX(verticales[j].cantCarriles);
-            //if(verticales.length != j){
-            //    this.stage.addChild(new CnvInterseccion(posx-largo/2,posy,horizontales[i].cantCarriles,verticales[j].cantCarriles));
-            //    generarCnvInterseccion(horizontales[i].cantCarriles,verticales[j].cantCarriles);
-            //}
+        for (var j = 0; j < cuadras.length; j++) {
+            stage.addChild(generarCuadra(HORIZONTAL,cantCarriles));
+            if(j!=cuadras.length-1)
+                actualizarPosX(verticales[j].cantCarriles);
         }
-        this.stage.addChild(new CnvCuadraReproductor(posx,posy,largo,horizontales[i].cantCarriles,"#b3b3b3",HORIZONTAL));
-        actualizarPosY(horizontales[i].cantCarriles);
+        actualizarPosY(cantCarriles);
     }
+    stage.canvas.width = posx + largo;
 
     moverPosxAlOrigen();
     moverPosyAlOrigen();
     posx=posx + largo;
     for (var i = 0; i < verticales.length; i++) {
+        var calle = verticales[i];
+        var cantCarriles = calle.cantCarriles;
+        var cuadras = calle.cuadras;
         moverPosyAlOrigen();
-        for (var j = 0; j < horizontales.length; j++) {
-            // actualizarPosY();
-            //posx=posx + this.largo;
-            stage.addChild(new CnvCuadraReproductor(posx,posy,largo,verticales[i].cantCarriles,"#b3b3b3",VERTICAL));
-            //var cnvCuadra = generarCuadra(VERTICAL,verticales[i].cantCarriles);
-            actualizarPosY(horizontales[j].cantCarriles);
+        for (var j = 0; j < cuadras.length; j++) {
+            stage.addChild(generarCuadra(VERTICAL,cantCarriles));
+            if(j!=cuadras.length-1)
+                actualizarPosY(horizontales[j].cantCarriles);
         }
-        actualizarPosX(verticales[i].cantCarriles);
+        actualizarPosX(cantCarriles);
+    }
+    stage.canvas.height = posy + largo;
+
+    function generarCnvInterseccion(cantCarrilesH,cantCarrilesV) {
+        var interseccion = new CnvInterseccion(posx,posy,cantCarrilesH,cantCarrilesV);
+        stage.addChild(interseccion);
     }
 
+    function generarCuadra(direccion,cantCarriles){
+        return new CnvCuadraReproductor(posx,posy,largo,cantCarriles,"#b3b3b3",direccion);
+    }
 
+    function actualizarPosX(cantCarriles){
+        posx = posx + largo + separador*cantCarriles +ala;
+    }
 
-        function generarCnvInterseccion(cantCarrilesH,cantCarrilesV) {
-            var interseccion = new CnvInterseccion(posx,posy,cantCarrilesH,cantCarrilesV);
-            stage.addChild(interseccion);
-        }
+    function actualizarPosY(cantCarriles){
+        posy = posy + largo + separador*cantCarriles +ala;
+    }
 
-        function generarCuadra(direccion,cantCarriles){
-            stage.addChild(new CnvCuadraReproductor(posx,posy,largo,cantCarriles,"#b3b3b3",direccion));
-        }
+    function moverPosxAlOrigen(){
+        posx = posInicialX;
+    }
 
-        function actualizarPosX(cantCarriles){
-            posx = posx + largo + separador*cantCarriles +ala;
-        }
-
-        function actualizarPosY(cantCarriles){
-            posy = posy + largo + separador*cantCarriles +ala;
-        }
-
-        function moverPosxAlOrigen(){
-            posx = posInicialX;
-        }
-
-        function moverPosyAlOrigen(){
-            posy = posInicialY;
-        }
-
-
-
+    function moverPosyAlOrigen(){
+        posy = posInicialY;
+    }
 };
