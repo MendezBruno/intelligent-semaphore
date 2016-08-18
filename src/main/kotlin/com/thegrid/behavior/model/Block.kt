382cd616c5a00325ae1adec19740eb72c032a7b0
Cristian Erik Ames Masek <cristianames.92@gmail.com>
2016-08-18 01:15:16 -0300

    Add block interface to behavior

--- src/main/kotlin/com/thegrid/behavior/model/Block.kt
+++ src/main/kotlin/com/thegrid/behavior/model/Block.kt
@@ -15,13 +15,45 @@ data class Block(
         val egressNode: NodeType) {
 
     private val _carCapacity: Int = 3 //TODO calcular segun length
-    private var _backStraightCarAmount: Int = 0
-    private var _backBendCarAmount: Int = 0
-    private var _frontStraightCarAmount: Int = 0
-    private var _frontBendCarAmount: Int = 0
+    private var _incomingCarsAmount: Int = 0
+    private var _outgoingLeftCarsAmount: Int = 0
+    private var _outgoingRightCarsAmount: Int = 0
+    private val _incomingCarsAvailability: Int =  _carCapacity / 2 - _incomingCarsAmount
+    private val _outgoingCarsAvailability: Int = _carCapacity / 2 - _outgoingLeftCarsAmount - _outgoingRightCarsAmount
 
     fun hasVerticalDirection() : Boolean{
         return street.orientation == Orientation.South || street.orientation == Orientation.North
     }
+
+    fun work() {
+        takeCarsOut()
+        moveCarsToTheFront()
+    }
+
+    private fun takeCarsOut(): Int {
+        val leftLeavingAmount = min(_outgoingLeftCarsAmount, egressNode.leftAvailability)
+        egressNode.takeLeftCars(leftLeavingAmount)
+        _outgoingLeftCarsAmount -= leftLeavingAmount
+
+        val rightLeavingAmount = min(_outgoingRightCarsAmount, egressNode.rightAvailability)
+        egressNode.takeRightCars(rightLeavingAmount)
+        _outgoingRightCarsAmount -= rightLeavingAmount
+    }
+
+    private fun moveCarsToTheFront() {
+        val totalAmount: Int
+        if (_incomingCarsAmount <= _outgoingCarsAvailability) {
+            totalAmount = _incomingCarsAmount
+            _incomingCarsAmount = 0
+        } else {
+            _incomingCarsAmount -= _outgoingCarsAvailability
+            totalAmount = _outgoingCarsAvailability
+        }
+        val leftAmount = (totalAmount * 0.5).toInt()
+        _outgoingLeftCarsAmount += leftAmount
+        _outgoingRightCarsAmount += totalAmount - leftAmount
+    }
+
+    fun min(value1: Int, value2: Int) = if (value1 <= value2) value1 else value2
 
 }
