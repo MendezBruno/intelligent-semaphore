010d52784e097ca01bd959e5a83fb3f3e8f5f9ba
Ezequiel Ayzenberg <Ezequiel Ayzenberg>
2016-08-21 20:45:49 -0300

    SimulationMock con factory method singleton

--- src/main/kotlin/com/thegrid/behavior/model/SimulationMock.kt
+++ src/main/kotlin/com/thegrid/behavior/model/SimulationMock.kt
@@ -1,12 +1,31 @@
 package com.thegrid.behavior.model
 
+import com.thegrid.behavior.extension.MapStateMemory
 import com.thegrid.communication.model.Map
 
 class SimulationMock {
 
     private var _map : Map;
+    private var _memory: MapStateMemory
 
-    constructor(map : Map) {
+    private constructor(map : Map) {
         _map = map;
+        _memory = MapStateMemory(map);
+    }
+
+    public fun getMap(): Map {
+        return _map;
+    }
+
+    public fun getMemory(): MapStateMemory {
+        return _memory;
+    }
+
+    companion object {
+        var SharedInstance: SimulationMock? = null;
+
+        public fun loadSimulation(map : Map) {
+            SharedInstance = SimulationMock(map);
+        }
     }
 }
