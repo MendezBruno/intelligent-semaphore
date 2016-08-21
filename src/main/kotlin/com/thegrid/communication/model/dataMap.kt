05fe232db82d91505f34857b504b6c30bc9f1ed1
Ezequiel Ayzenberg <Ezequiel Ayzenberg>
2016-08-21 13:12:11 -0300

    Fix project (agrego Kotson) + Fix test

--- src/main/kotlin/com/thegrid/communication/model/dataMap.kt
+++ src/main/kotlin/com/thegrid/communication/model/dataMap.kt
@@ -4,10 +4,12 @@ import com.google.api.server.spi.config.ApiResourceProperty
 
 class dataMap{
     public var nombre = "";
-    public var callesHorizontales: Array<dataStreet> = arrayOf();
-    public var callesVerticales: Array<dataStreet> = arrayOf();
-    public var nodosEntrada: Array<dataEdgeNode> = arrayOf();
-    public var nodosSalida: Array<dataEdgeNode> = arrayOf();
-    public var nodosSemaforo: Array<dataSemaphoreNode> = arrayOf();
-    public var nodosNoSemaforo: Array<dataControlNode> = arrayOf();
+    public var callesHorizontales: MutableList<dataStreet> = mutableListOf();
+    public var callesVerticales: MutableList<dataStreet> = mutableListOf();
+    public var nodosEntrada: MutableList<dataEdgeNode> = mutableListOf();
+    public var nodosSalida: MutableList<dataEdgeNode> = mutableListOf();
+    public var nodosSemaforo: MutableList<dataSemaphoreNode> = mutableListOf();
+    public var nodosNoSemaforo: MutableList<dataControlNode> = mutableListOf();
+
+    companion object
 }
--- src/main/kotlin/com/thegrid/communication/model/dataStreet.kt
+++ src/main/kotlin/com/thegrid/communication/model/dataStreet.kt
@@ -4,6 +4,6 @@ class dataStreet {
     public var cantCarriles:Int=1;
     public var preferencia:Int=0;
     public var sentido:String="";
-    public var sentidosPosibles:Array<String> = arrayOf();
-    public var cuadras:Array<dataBlock> = arrayOf();
+    public var sentidosPosibles: MutableList<String> = mutableListOf();
+    public var cuadras:MutableList<dataBlock> = mutableListOf();
 }
