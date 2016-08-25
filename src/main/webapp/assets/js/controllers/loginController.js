fbd1d075a2a602c8597765e56dc7fd6cdba8cb34
mariano <marianoguido@yahoo.com.ar>
2016-08-25 02:44:04 -0300

    Generado mock usuario
    usu: admin@hotmail.com
    
    pass: admin

--- src/main/webapp/assets/js/controllers/loginController.js
+++ src/main/webapp/assets/js/controllers/loginController.js
@@ -1,9 +1,42 @@
 /**
  * Created by bruno on 26/07/16.
  */
-app.controller("loginController", function($scope){
+app.controller("loginController", function($scope,$location){
 
     console.log("hola");
 
+    $scope.entro=function () {
+
+        if ($scope.mail== "admin@hotmail.com")
+        {
+
+
+            if ($scope.pass=="admin")
+            {
+
+                $location.url("/app/galeria")
+
+
+            }
+            else
+
+            {
+
+
+                alert("Contraseña o usuario mal ingresados");
+            }
+
+
+        }
+        else
+        {
+
+            alert("Contraseña o usuario mal ingresados");
+
+        }
+
+
+
+    }
 
 });
