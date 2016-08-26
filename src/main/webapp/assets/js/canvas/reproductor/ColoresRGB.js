c1384c49aea68f580451388e87b38eebf7ffdb04
eayzenberg <eayzenberg>
2016-08-26 12:58:01 -0300

    Agrego funcion toRGBA y redefino toHexa

--- src/main/webapp/assets/js/canvas/reproductor/ColoresRGB.js
+++ src/main/webapp/assets/js/canvas/reproductor/ColoresRGB.js
@@ -19,9 +19,19 @@ function ColoresRGB(r,g,b) {
  * @returns {String} Equivalente hexa de los colores rgb
  */
 ColoresRGB.prototype.toHexa = function() {
+    return "#" +
+    ("0" + parseInt(this.r,10).toString(16)).slice(-2) +
+    ("0" + parseInt(this.g,10).toString(16)).slice(-2) +
+    ("0" + parseInt(this.b,10).toString(16)).slice(-2);
+};
+
+/**
+ * @returns {String} Equivalente rgba del objeto.
+ */
+ColoresRGB.prototype.toRGBA = function () {
     return createjs.Graphics.getRGB(
         parseInt(this.r),parseInt(this.g),parseInt(this.b),1);
-};
+}
 
 /**
  * @returns {string} Un string apropiado para la propiedad _fill.style
@@ -57,4 +67,8 @@ ColoresRGB.getSKYBLUE = function () {
  */
 ColoresRGB.getYELLOW = function () {
     return new ColoresRGB(254,255,148);
+}
+
+ColoresRGB.getPANEL_GRAY = function () {
+    return new ColoresRGB(227,227,227);
 }
