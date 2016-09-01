1bdc4f2cc172e8b7fb3b74eb4877890cdb8370f4
bruno <brunomendez88@hotmail.com>
2016-09-01 02:50:57 -0300

    agrego las flechas, agrego rotation a las flechas, pero todavia no rota bien.

--- src/main/webapp/assets/js/canvas/constructors/CnvNodoBorde.js
+++ src/main/webapp/assets/js/canvas/constructors/CnvNodoBorde.js
@@ -1,4 +1,4 @@
-    function CnvNodoBorde(nodo,fila,columna, posx, posy,radio,color) {
+    function CnvNodoBorde(nodo,fila,columna, posx, posy,radio,color,sentido) {
         this.Container_constructor();
 
         this.fila = fila;
@@ -6,6 +6,8 @@
         this.id = nodo.id;
         this.nodo = nodo;
         this.color = color;
+        this.flecha;
+        this.sentido = sentido;
         this.posx = posx;
         this.posy = posy;
         this.radio = radio;
@@ -29,12 +31,27 @@
 
     //METODOS DEL CONSTRUCTOR
     CnvNodoBorde.prototype.setup = function() {
+        //Dibujo el nodo
         this.background = new createjs.Shape();
         this.background.graphics
             .beginFill(this.color)
             .drawCircle(this.posx,this.posy,this.radio);
-
         this.addChild(this.background);
+
+        //Dibujo la flecha
+        var pathFlecha = rutaDeLaFlechaSegunSentido(this.sentido);  //*TODO* Eze: traer del objeto global de sentidos. esto se encuentra en utils.js
+        this.flecha = new createjs.Bitmap(pathFlecha);
+        this.flecha.scaleX=0.1;
+        this.flecha.scaleY=0.1;
+        this.flecha.x=this.posx-5;
+        this.flecha.y=this.posy-5;
+        this.addChild(this.flecha);
+
+
+        //escribo la orientacion
+
+
+
         this.on("click", this.handleClick);
         this.on("rollover", this.handleRollOver);
         this.on("rollout", this.handleRollOver);
@@ -44,6 +61,24 @@
 
         this.offset = Math.random()*10;
         this.count = 0;
+
+        function rutaDeLaFlechaSegunSentido(unSentido){
+            switch(unSentido) {
+                case 'Este-Oeste':
+                    return '/assets/img/flechaIzquierda.jpeg';
+                    break;
+                case 'Norte-Sur':
+                    return '/assets/img/flechaAbajo.jpeg';
+                    break;
+                case 'Oeste-Este':
+                    return '/assets/img/flechaDerecha.jpeg';
+                    break;
+                case 'Sur-Norte':
+                    return '/assets/img/flechaArriba.jpeg';
+                    break;
+
+            }
+        }
     } ;
 
     CnvNodoBorde.prototype.handleRollOver = function(event) {
@@ -55,6 +90,12 @@
         this.background.graphics.clear()
             .beginFill(this.color)
             .drawCircle(this.posx,this.posy,this.radio);
+        this.cambiarSentido();
+    }
+
+    CnvNodoBorde.prototype.cambiarSentido = function(){
+        this.flecha.rotation=180;
+
     }
 
     window.CnvNodoBorde = createjs.promote(CnvNodoBorde, "Container");
