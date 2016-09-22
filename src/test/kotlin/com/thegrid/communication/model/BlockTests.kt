1758d221e0589e782c2ece2955c423b115c15d0e
eayzenberg <eayzenberg>
2016-09-22 13:42:31 -0300

    Adapto test

--- src/test/kotlin/com/thegrid/communication/model/BlockTests.kt
+++ src/test/kotlin/com/thegrid/communication/model/BlockTests.kt
@@ -3,6 +3,9 @@ package com.thegrid.communication.model
 import com.github.salomonbrys.kotson.*
 import com.google.gson.Gson
 import com.thegrid.behavior.model.*
+import com.thegrid.behavior.platform.IDispatcheable
+import com.thegrid.behavior.services.EventList
+import com.thegrid.behavior.services.model.PairDispatched
 import com.thegrid.communication.services.MapConversor
 import org.jetbrains.spek.api.Spek
 
@@ -16,12 +19,16 @@ class BlockTests: Spek({
                 val fin = EgressNode("nodox",10,5)
                 val block = Block("", street, 0, semaphoreNode, fin)
 
-                block.executeEvent(0.0, _futureEventsTable)
-                semaphoreNode.executeEvent(5.0, _futureEventsTable)
-                block.executeEvent(10.0, _futureEventsTable)
-                semaphoreNode.executeEvent(12.0, _futureEventsTable)
-                semaphoreNode.executeEvent(14.0, _futureEventsTable)
-                block.executeEvent(16.0, _futureEventsTable)
+                val TEF = EventList<PairDispatched<IDispatcheable>>()
+                TEF.add(PairDispatched(0.0,block))
+                TEF.add(PairDispatched(0.0,semaphoreNode))
+
+                block.executeEvent(0.0, TEF)
+                semaphoreNode.executeEvent(5.0, TEF)
+                block.executeEvent(10.0, TEF)
+                semaphoreNode.executeEvent(12.0, TEF)
+                semaphoreNode.executeEvent(14.0, TEF)
+                block.executeEvent(16.0, TEF)
             }
         }
     }
