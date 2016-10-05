379d7fe1a4fe2ea12bfba41a29d6861b1957b11e
eayzenberg <eayzenberg>
2016-10-05 12:21:45 -0300

    Galeria y preview optimizadas

--- src/main/webapp/assets/js/controllers/previewController.js
+++ src/main/webapp/assets/js/controllers/previewController.js
@@ -1,46 +1,13 @@
-app.controller('previewController', function($scope,Mapa,MyService,$routeParams,$location,$timeout,$rootScope) {
-
-
-    pedirMapasFirebase = function () {
-        var mapasPreview = {};
-        firebase.database().ref('/' + 'pepe/mapas' ).once('value', function(snapshot) {
-
-            var json_mapas= snapshot.val();
-
-            for (var mapa1 in json_mapas) {
-                mapasPreview[json_mapas[mapa1].nombre]=json_mapas[mapa1];
-            }
-
-
-    var largo = 30;
-    console.log("hola");
-    console.log($rootScope.mapeos);
-    console.log($scope.algo);
+app.controller('previewController', function($scope,Mapa,MyService,$routeParams,$location,$timeout) {
     // var mapa = $routeParams.id ? mapasPreview[$routeParams.id]:mapas["modulo1"];
-    var modelo1 = MapaEditor.desParsear(JSON.stringify(mapasPreview[$routeParams.id]));
+    var mapa = parent.json_mapas[$routeParams.id];
+    var modelo1 = MapaEditor.desParsear(JSON.stringify(mapa));
     $scope.callesV= modelo1.callesHorizontales[0].cuadras.length - 1;
     $scope.callesH= modelo1.callesVerticales[0].cuadras.length - 1;
     $scope.nombre=modelo1.nombre;
     $scope.intervalo = 5; //se puede settear en el reproductor antes de arrancar
     var stage = new createjs.Stage("mapa");
-    var logica = new GrillaController(3,3,largo,stage,$scope,$timeout);
-    createjs.Ticker.on("tick", stage);
+    var logica = new GrillaController(3,3,30,stage,$scope,$timeout);
     logica.setModelo(modelo1);
     logica.redibujar();
-    document.body.style.overflow = 'hidden';
-    document.getElementById("preview").style.width = "100%"
-    document.getElementById("preview").style.top = "0px"
-    document.getElementById("preview").style.left = "0px"
-
-        });
-
-    };
-
-    pedirMapasFirebase();
-
-
-
-
-
-
 });
